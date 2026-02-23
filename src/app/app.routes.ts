da9c32b4ec0adce9bc53cf55e5c504be7c4bb6c4
codebyallan <allan.pereira.dev@proton.me>
2026-02-23 12:33:35 -0300

    feat(layout): implement responsive main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { DefaultLayout } from './layout/default-layout/default-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: DefaultLayout, children: [] }];
---
+++ src/app/layout/default-layout/default-layout.ts
@@ -0,0 +1,23 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatDrawerContainer, MatDrawer, MatDrawerContent } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-default-layout',
+  imports: [MatDrawerContainer, MatDrawer, MatDrawerContent, MatToolbar, MatButtonModule, MatIcon, RouterOutlet],
+  templateUrl: './default-layout.html',
+  styleUrl: './default-layout.scss',
+})
+export class DefaultLayout implements OnInit {
+  private breakpointObserver = inject(BreakpointObserver);
+  protected isMobile = signal<boolean>(false);
+  ngOnInit() {
+    this.breakpointObserver.observe(Breakpoints.Handset).subscribe(result => {
+      this.isMobile.set(result.matches);
+    });
+  }
+}
